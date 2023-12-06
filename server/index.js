cecda99bf5065373fcdad69f7c93931f5b8923d6
Gauravzz12 <gaurav1234thakurgt@gmail.com>
2023-12-06 15:55:54 +0530

    Initial Commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var app = express();
+var cors = require("cors"); // Import the cors middleware
+var connection = require("./database");
+
+app.use(cors()); // Enable CORS for all routes
+
+app.get("/", function (req, res) {
+  let sql = "Select * from pets";
+  connection.query(sql, function (err, results) {
+    res.send(results);
+  });
+});
+
+app.listen(4000, function () {
+  console.log("App Listening on port 3000");
+  connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Database Connected");
+  });
+});
