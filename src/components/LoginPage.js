eae67e019e738eb0a5f799c6144c10eece3ae75b
Gauravzz12 <gaurav1234thakurgt@gmail.com>
2023-12-07 10:51:23 +0530

    Login Page and reactBootsrap

---
+++ src/components/LoginPage.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+export default function LoginPage() {
+    return (
+        <div>
+            {/* bg-color #9A616D*/}
+            <section className="vh-100" style={{ backgroundColor: 'white' }}>
+                <div className="container py-5 h-100" style={{ marginTop:'-50px',height:'80vh'}}>
+                    <div className="row d-flex justify-content-center align-items-center h-100">
+                        <div className="col-md-6 col-lg-5">
+                            {/* Image Section */}
+                            <img src="./images/loginPageImage.jpg" alt="login form" className="img-fluid" style={{ borderRadius: '1rem 0 0 1rem', height: '80vh', width: '100%',marginBottom:'150px' }} />
+                        </div>
+
+                        <div className="col-md-6 col-lg-7">
+                            {/* Form Section */}
+                            {/* style={{ borderRadius: '0 1rem 1rem 0' }} */}
+                            <div className="card h-100" >
+                                <div className="card-body p-4 p-lg-5 text-black d-flex align-items-center" style={{marginTop:'-140px'}}>
+                                    <form className="w-100">
+                                        <div className="text-center mb-5">
+                                            <i className="fas fa-cubes fa-2x me-3" style={{ color: '#ff6219' }}></i>
+                                            <img
+                                                alt="GETaPET-LOGO"
+                                                src="/images/Logo.jpg"
+                                                width="100"
+                                                height="100"
+                                                padding="-10px"
+                                                className="d-inline-block align-top"
+                                            />
+                                        </div>
+
+                                        <h5 className="fw-normal mb-3 pb-3" style={{ letterSpacing: '1px' }}>WELCOME BACK !</h5>
+
+                                        <div className="form-outline mb-4">
+                                            <input type="email" id="form2Example17" className="form-control form-control-lg" />
+                                            <label className="form-label" htmlFor="form2Example17">Email address</label>
+                                        </div>
+
+                                        <div className="form-outline mb-4">
+                                            <input type="password" id="form2Example27" className="form-control form-control-lg" />
+                                            <label className="form-label" htmlFor="form2Example27">Password</label>
+                                        </div>
+
+                                        <div className="pt-1 mb-4">
+                                            <button className="btn btn-dark btn-lg btn-block" type="button">Login</button>
+                                        </div>
+
+                                        <p className="mb-2" style={{ color: '#393f81', textAlign: 'center' }}>Forgot password? Don't have an account? <a href="#!" style={{ color: '#393f81' }}>Register here</a></p>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,39 +5,41 @@ import { Navbar, Nav, Container } from 'react-bootstrap';
 function NavBar() {
   return (
     <div>
-    <Navbar bg="light" expand="lg" >
-      <Container fluid>
-        <Link to="/" className="Logo">
-          <img
-            alt=""
-            src="/images/Logo.jpg"
-            width="100"
-            height="100"
-            className="d-inline-block align-top logo-image"
-          />
-        </Link>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav className="me-auto my-2 my-lg-0 navbar-nav-scroll">
-            <Nav.Link as={Link} to="/" className="Links">
-              HOME
-            </Nav.Link>
-            <Nav.Link as={Link} to="/Pets" className="Links">
-              PETS
-            </Nav.Link>
-            <Nav.Link as={Link} to="/Adopt" className="Links">
-              ADOPT
-            </Nav.Link>
-            <Nav.Link as={Link} to="/About" className="Links">
-              ABOUT US
-            </Nav.Link>
-          </Nav>
-          <Nav>
-            <button className="btn btn-outline-success">LOGIN</button>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+      <Navbar bg="light" expand="lg">
+        <Container fluid>
+          <Link to="/" className="Logo">
+            <img
+              alt=""
+              src="/images/Logo.jpg"
+              width="100"
+              height="100"
+              className="d-inline-block align-top logo-image"
+            />
+          </Link>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav className="me-auto my-2 my-lg-0 navbar-nav-scroll">
+              <Nav.Link as={Link} to="/" className="Links">
+                HOME
+              </Nav.Link>
+              <Nav.Link as={Link} to="/Pets" className="Links">
+                PETS
+              </Nav.Link>
+              <Nav.Link as={Link} to="/Adopt" className="Links">
+                ADOPT
+              </Nav.Link>
+              <Nav.Link as={Link} to="/About" className="Links">
+                ABOUT US
+              </Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link as={Link} to="/Login" className="btn btn-outline-success">
+                LOGIN
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 }
